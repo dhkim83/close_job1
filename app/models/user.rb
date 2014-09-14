d75946c16a5f0b5d852b89d26144f9bd9faa126c
Dohee <dhkim83@gmail.com>
2014-09-14 15:23:38 -0700

    Add account activation email and users signup test updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,13 +24,27 @@ class User < ActiveRecord::Base
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end 
-   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    BCrypt::Password.new(digest).is_password?(token)
   end
+  
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+    # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver
   end
 
   private
